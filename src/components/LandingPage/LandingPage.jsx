2942a0440fa9cc2c2ed81b3d55f8b031fbbc734a
hasnatbajwa <hasnatjbajwa@gmail.com>
2022-11-08 12:17:15 +0500

    Developed Advertising Banner

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
 import Header from "../Header/Header";
+import Slider from "../../modules/Slider/Slider";
 const LandingPage = () => {
   return (
     <>
       <Navbar />
       <Header />
-  
+      <Slider/>
     </>
   );
 };
---
+++ src/modules/Slider/Slider.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+import SimpleImageSlider from "react-simple-image-slider";
+import image1 from "../../assets/1.jpg";
+import image2 from "../../assets/2.jpg";
+import image3 from "../../assets/3.jpg";
+import image4 from "../../assets/4.jpg";
+
+import "./sliderStyles.css";
+
+const Slider = () => {
+  const Images = [{ url: image4},{ url: image2 },{ url: image1 }, { url: image3 }, ];
+  return (
+    <Grid container justify="center">
+      <div className="slider">
+          <SimpleImageSlider
+            width={1200}
+            height={200}
+            images={Images}
+            showBullets={true}
+            showNavs={true}
+            autoPlay={true}
+            slideDuration={0.7}
+          ></SimpleImageSlider>
+      </div>
+    </Grid>
+  );
+};
+
+export default Slider;
